58105f59cd8577d8974d4c1d01bc58e9ae7dee8f
Duncan <duncantheinvictus@gmail.com>
2022-07-04 17:43:07 +0800

    Select people is ready
    there's a margin issue with SelectDate.

--- src/selectPeople.js
+++ src/selectPeople.js
@@ -1,7 +1,37 @@
 import './App.css'
+import React, { useState } from 'react';
+import Select from 'react-select';
+
 
 function SelectPeople () {
+   
+  const options = [
+    { value: '1', label: '1' },
+    { value: '2', label: '2' },
+    { value: '3', label: '3' },
+    { value: '4', label: '4' },
+    { value: '5', label: '5' },
+    { value: '6', label: '6' },
+    { value: '7', label: '7' },
+    { value: '8', label: '8' },
+    { value: '9', label: '9' },
+    { value: '10', label: '10' },
+  ]
+  const [selectedOption, setSelectedOption] = useState(null);
 
+  // const onchange = (e) => {
+  //   setSelectedOption(e.target.value);
+  // }
+return(
+  // <div/>
+  <div className=" w-10w h-11 relative rounded-xl text-sm font-semibold flex flex-row justify-start items-center pl-3 ">
+      <Select
+        defaultValue={selectedOption}
+        onChange={setSelectedOption}
+        options={options}
+      />
+    </div>
+  )
 }
 
 export default SelectPeople
--- src/selectTime.js
+++ src/selectTime.js
@@ -1,7 +1,25 @@
 import './App.css'
+import { useState } from 'react';
+import { Button, TimePicker } from 'antd';
+import 'antd/dist/antd.css';
+
 
 function SelectTime () {
-
+    const [open, setOpen] = useState(false);
+    return (
+            <TimePicker
+                open={open}
+                onOpenChange={setOpen}
+                className='w-20w h-11 relative !border-hidden !rounded-xl text-sm font-semibold flex flex-row justify-start items-center '
+                renderExtraFooter={() => (
+                <Button size="small" type="primary" onClick={() => setOpen(false)}>
+                    OK
+                </Button>
+                )}
+            />
+      
+    );
+  
 }
 
 export default SelectTime
